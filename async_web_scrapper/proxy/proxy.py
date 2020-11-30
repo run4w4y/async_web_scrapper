42143ad5a5c55183df1076e8d4adcea1d9f2e95c
Vadim Makarov <add4che@gmail.com>
2020-11-30 23:32:07 +0300

    update Proxy

--- async_web_scrapper/proxy/proxy.py
+++ async_web_scrapper/proxy/proxy.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from async_web_scrapper import GenericItem
+import logging
+import httpx
 
 
 class Proxy(GenericItem, ABC):
@@ -37,20 +39,21 @@ class Proxy(GenericItem, ABC):
     # checks if proxy is available
     async def check(self) -> bool:
         try:
-            async with httpx.AsyncClient(proxies=self.to_dict()) as client:
+            async with httpx.AsyncClient(proxies=self.to_httpx()) as client:
                 r = await client.get(self.PING_URL, timeout=20)
                 
             logging.info(f'Proxy {self} is available')
             return True
         except httpx.HTTPError as e:
-            logging.warning(f'Proxy {self} seems to be unavailable [{type(e)}]')
+            logging.warning(f'Proxy {self} seems to be unavailable, error: {str(e)}')
             return False
 
     # converts proxy to a dict usable with requests
-    def to_dict(self) -> dict:
-        return {
-            self.PROXY_PROTOCOL: f'{self.PROXY_PROTOCOL}://{self.ip}:{self.port}'
-        }
+    def to_httpx(self):
+        return httpx.Proxy(
+            url = f'{self.PROXY_PROTOCOL}://{self.ip}:{self.port}',
+            mode = 'FORWARD_ONLY'
+        )
     
     # converts proxy to a csv row
     def to_csv_row(self) -> list:
