57c026aa54e056645709353826b51eaa0961a281
Vadim Makarov <add4che@gmail.com>
2020-11-30 23:30:13 +0300

    pass downloader and proxy_pool into GenericPage

--- async_web_scrapper/generic_page.py
+++ async_web_scrapper/generic_page.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
+from .file_downloader import AsyncFileDownloader
 
 
 class GenericPage(ABC):
-    def __init__(self, url: str):
+    def __init__(self, url: str, proxy_pool = None, downloader: AsyncFileDownloader = None):
         self.url = url
+        self.proxy_pool = proxy_pool
+        self.downloader = downloader
     
     @property
     @abstractmethod
