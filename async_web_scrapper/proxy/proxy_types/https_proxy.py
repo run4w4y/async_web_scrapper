d9ae5cdfe84d3e3f5b161c3f535aa4304433810b
Vadim Makarov <add4che@gmail.com>
2020-11-30 23:31:51 +0300

    dirty fix for protocol in HTTPSProxy

--- async_web_scrapper/proxy/proxy_types/https_proxy.py
+++ async_web_scrapper/proxy/proxy_types/https_proxy.py
@@ -1,3 +1,4 @@
+import httpx
 from ..proxy import Proxy
 
 
@@ -5,9 +6,9 @@ class HTTPSProxy(Proxy):
     @property
     def PROXY_PROTOCOL(self):
         return "https"
-    
-    def to_dict(self):
-        return {
-            'http': f'http://{self.ip}:{self.port}',
-            'https': f'https://{self.ip}:{self.port}'
-        }
+
+    def to_httpx(self):
+        return httpx.Proxy(
+            url = f'http://{self.ip}:{self.port}',
+            mode = 'TUNNEL_ONLY'
+        )
