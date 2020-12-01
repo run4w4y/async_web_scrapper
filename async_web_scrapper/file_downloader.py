7168655a3b6f705d3ca8cdfb1c3ede8c95307a94
Vadim Makarov <add4che@gmail.com>
2020-12-01 03:15:35 +0300

    fixed bugs in AsyncFileDownloader

--- async_web_scrapper/file_downloader.py
+++ async_web_scrapper/file_downloader.py
@@ -7,14 +7,14 @@ import logging
 
 
 class AsyncFileDownloader:
-    def __init__(self, save_path: str = 'donwloads/', workers_amount: int = 10):
+    def __init__(self, save_path: str = 'downloads/', workers_amount: int = 10):
         logging.info('File downloader started')
         self.save_path = save_path
         os.makedirs(self.save_path, exist_ok=True)
         self.workers_amount = workers_amount
 
         self.__download_queue = asyncio.Queue(maxsize=self.workers_amount)
-        self.tasks_dispatched = [self._downloader_dispatched(i) for i in range(self.workers_amount)]
+        self.tasks_dispatched = [asyncio.create_task(self._downloader_dispatched(i)) for i in range(self.workers_amount)]
     
     async def add_download(self, url: str):
         await self.__download_queue.put(url)
@@ -25,8 +25,9 @@ class AsyncFileDownloader:
             logging.info(f'Downloading image {url}')
             filename = url.split('/')[-1]
 
-            r = await httpx.get(url)
-            r.raise_for_status()
+            async with httpx.AsyncClient() as client:
+                r = await client.get(url)
+                r.raise_for_status()
 
             async with aiofiles.open(os.path.join(self.save_path, filename), 'wb') as f:
                 await f.write(r.content)
