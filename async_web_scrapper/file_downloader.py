60f2dcc6f8afdc4c8e6ac27f811d7bcc1af37018
Vadim Makarov <add4che@gmail.com>
2020-12-02 13:02:58 +0300

    a few fixes in AsyncFileDownloader

--- async_web_scrapper/file_downloader.py
+++ async_web_scrapper/file_downloader.py
@@ -13,13 +13,14 @@ class AsyncFileDownloader:
         os.makedirs(self.save_path, exist_ok=True)
         self.workers_amount = workers_amount
 
-        self.__download_queue = asyncio.Queue(maxsize=self.workers_amount)
+        self.__download_queue = asyncio.Queue()
         self.tasks_dispatched = [asyncio.create_task(self._downloader_dispatched(i)) for i in range(self.workers_amount)]
     
     async def add_download(self, url: str):
         await self.__download_queue.put(url)
     
     async def _downloader_dispatched(self, number: int):
+        logging.info(f'Started dispatched downloader {number}')
         while True:
             url = await self.__download_queue.get()
             logging.info(f'Downloading {url}')
