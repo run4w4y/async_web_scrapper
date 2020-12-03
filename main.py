cd2ef658cea58247bce2614eb3587a90aad309dd
Vadim Makarov <add4che@gmail.com>
2020-12-03 04:50:51 +0300

    update main to work with any of the drivers

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
+import sys
+import getopt
 import asyncio
 import logging
-from async_web_scrapper.proxy import ProxyScrapper, ProxyPool
-from kaggle_social_driver import KaggleSocialScrapper 
 
 async def main():
-    proxy_pool = ProxyPool(scrapper=ProxyScrapper())
-    scrapper = KaggleSocialScrapper(csvpath='kaggle_social/users.csv', proxy_pool=proxy_pool)
-    await scrapper.result
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], '', ['driver=', 'shopname=', 'baseurl=', 'useproxy'])
+    except getopt.GetoptError:
+        logging.error('Specify options properly')
+        sys.exit(1)
+
+    optdict = dict(opts)
+    driver_str = optdict.get('--driver')
+    if driver_str is None:
+        logging.error('You must specify the --driver option')
+        sys.exit(1)
+    
+    driver_module = None
+    try:
+        driver_module = eval(f'__import__("drivers.{driver_str}", fromlist=["{driver_str}"])') 
+    except BaseException as e:
+        logging.error(e)
+        sys.exit(1)
+
+    await driver_module.main(args, optdict)
 
 if __name__ == '__main__':
     asyncio.log.logger.setLevel(logging.ERROR)
-    logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.INFO)
+    logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s', level=logging.INFO)
     logging.info('Starting scrapper')
     
     try:
