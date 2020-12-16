49a0b98bff341b872649310fac8ce439c0709171
Vadim Makarov <add4che@gmail.com>
2020-12-16 19:58:00 +0300

    make csv writer easier to inherit

--- async_web_scrapper/csv_writer.py
+++ async_web_scrapper/csv_writer.py
@@ -21,18 +21,24 @@ class AsyncCSVWriter:
 
         self.csvpath = csvpath
         self.__write_queue = asyncio.Queue()
-        asyncio.create_task(self._start_writer())
+        self._writer_task = asyncio.create_task(self._start_writer())
     
     # append item to the csv
     async def add_item(self, item: GenericItem):
         await self.__write_queue.put(item)
+    
+    async def _write(self, item):
+        async with aiofiles.open(self.csvpath, 'a', newline='') as csvfile:
+            buffer = io.StringIO()
+            writer = csv.writer(buffer)
+            writer.writerow(item.to_csv_row())
+            await csvfile.write(buffer.getvalue())
 
     async def _start_writer(self):
         logging.info(f'Started AsyncCSVWriter with csvpath={self.csvpath}')
         while True:
-            item = await self.__write_queue.get()
-            async with aiofiles.open(self.csvpath, 'a', newline='') as csvfile:
-                buffer = io.StringIO()
-                writer = csv.writer(buffer)
-                writer.writerow(item.to_csv_row())
-                await csvfile.write(buffer.getvalue())
+            try:
+                item = await self.__write_queue.get()
+                self._write(item)
+            except asyncio.CancelledError:
+                logging.info(f'AsyncCSVWriter(csvpath={self.csvpath}) done')
